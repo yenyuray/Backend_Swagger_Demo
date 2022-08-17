4f831279946dfefa875f9b0994030437f54c14bb
RobinChien <goodnana1224@gmail.com>
2022-08-17 14:43:24 +0800

    created_time to readonly

--- core/models.py
+++ core/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 
 class Order(models.Model):
     order_id = models.AutoField(auto_created=True, primary_key=True)
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(auto_now_add=True)
     last_update_date = models.DateTimeField(auto_now=True)
     list_of_items = models.TextField(max_length=1000)
